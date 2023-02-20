bfb6412d2bf06b067e29dd2286dfb4a8e25be365
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-02-19 21:11:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,16 @@ if uploaded_file is not None:
     # Analizar el sentimiento del texto del libro utilizando TextBlob
     blob = TextBlob(text)
     sentiment = blob.sentiment.polarity
-    # Generar una reseña basada en el sentimiento detectado
+    # Citas del libro
+    quotes = ["La literatura es el arte de escribir algo que se lee dos veces; y la poesía, algo que se lee una y otra vez.",
+              "Los libros siempre habían sido para mí una ventana a lugares maravillosos.",
+              "En el futuro, todo el mundo será mundialmente famoso durante quince minutos."]
+    # Generar una reseña citando partes del libro
     if sentiment > 0:
-        review = "Me gustó mucho este libro, ¡lo recomiendo!"
+        review = f"Me encantó este libro. La prosa es hermosa y las historias son profundas. Como dijo Andy Warhol: '{quotes[2]}'."
     elif sentiment < 0:
-        review = "No me gustó este libro, no lo recomendaría."
+        review = f"No me gustó este libro. La prosa es confusa y las historias son poco interesantes. Como dijo Oscar Wilde: '{quotes[0]}'."
     else:
-        review = "Este libro es neutral, no tengo una opinión fuerte al respecto."
+        review = f"Este libro es neutral, no tengo una opinión fuerte al respecto. Como dijo Marcel Proust: '{quotes[1]}'."
     # Mostrar la reseña generada al usuario
     st.write(review)

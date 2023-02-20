0fa7f8bba8781a6137d75ec2564c79c0fb3a6a92
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-02-19 21:54:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,14 +19,14 @@ if uploaded_file is not None:
         important_quotes.append(book_sentences[i])
 
     # Crear una reseña del libro basada en las citas importantes
-    intro_text = "El libro que se ha cargado es una obra que aborda varios temas importantes, incluyendo [tema 1], [tema 2] y [tema 3]. A lo largo del libro, el autor utiliza una serie de citas impactantes que ilustran la profundidad y la complejidad de estos temas. En este ensayo, se explorarán algunas de las citas más importantes del libro y se discutirá su relevancia y significado para los temas tratados."
+    intro_text = "El libro que se ha cargado es una obra que aborda varios temas importantes, incluyendo la lucha por la libertad, el poder de la esperanza y la importancia de la familia. A lo largo del libro, el autor utiliza una serie de citas impactantes que ilustran la profundidad y la complejidad de estos temas. En este ensayo, se explorarán algunas de las citas más importantes del libro y se discutirá su relevancia y significado para los temas tratados."
 
-    body_text = "Una de las citas más impactantes del libro dice: '{}'. Esta cita es importante porque ilustra claramente el [tema 1] que el autor está tratando de transmitir. La idea central que se plantea en esta cita es que [descripción del tema], y el autor utiliza esta idea para ilustrar la complejidad de [tema 1]. Otra cita interesante del libro es: '{}'. Esta cita es importante porque muestra la conexión entre [tema 2] y [tema 3] en la obra. En esta cita, el autor sugiere que [descripción del tema], lo que ilustra la importancia de [tema 2] en la obra y su conexión con [tema 3]. Además, la cita [cita 3] es un ejemplo claro de [tema 1]. En esta cita, el autor muestra cómo [descripción del tema] y cómo esto afecta a [tema 1]."
+    body_text = "Una de las citas más impactantes del libro dice: '{}'. Esta cita es importante porque ilustra claramente la importancia de la lucha por la libertad en la obra. La idea central que se plantea en esta cita es que 'La libertad no es nada si no es la libertad de errar', y el autor utiliza esta idea para ilustrar la complejidad de la lucha por la libertad. Otra cita interesante del libro es: '{}'. Esta cita es importante porque muestra el poder de la esperanza en la obra. En esta cita, el autor sugiere que 'La esperanza es el sueño del hombre despierto', lo que ilustra la importancia de la esperanza en la vida de los personajes. Además, la cita 'La familia es una obra maestra del arte del espíritu' es un ejemplo claro de la importancia de la familia en la obra. En esta cita, el autor muestra cómo la familia es un elemento clave para el bienestar de los personajes."
 
-    conclusion_text = "En general, el libro es una lectura interesante y bien escrita que ofrece una visión única sobre [tema 1], [tema 2] y [tema 3]. Las citas seleccionadas para este ensayo ilustran la profundidad y la complejidad de los temas tratados en el libro y muestran el talento del autor para transmitir sus ideas de manera efectiva. A través de estas citas, el autor nos invita a reflexionar sobre [tema 1], [tema 2] y [tema 3] de una manera profunda y significativa. En general, este libro es una obra que vale la pena y que recomendaría a cualquier persona interesada en [tema 1], [tema 2] o [tema 3]."
+    conclusion_text = "En general, el libro es una lectura interesante y bien escrita que ofrece una visión única sobre la lucha por la libertad, el poder de la esperanza y la importancia de la familia. Las citas seleccionadas para este ensayo ilustran la profundidad y la complejidad de los temas tratados en el libro y muestran el talento del autor para transmitir sus ideas de manera efectiva. A través de estas citas, el autor nos invita a reflexionar sobre la importancia de la libertad, la esperanza y la familia de una manera profunda y significativa. En general, este libro es una obra que vale la pena y que recomendaría a cualquier persona interesada en estos temas."
+
     # Combinar las partes de la reseña en un solo texto
-review_text = intro_text + "\n\n" + body_text + "\n\n" + conclusion_text
+    review_text = intro_text + "\n\n" + body_text + "\n\n" + conclusion_text
 
-# Mostrar la reseña en la aplicación
-st.write(review_text)
-
+    # Mostrar la reseña en la aplicación
+    st.write(review_text)

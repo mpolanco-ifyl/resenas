dd06e4df7ea57719de78f9d62f589180fc27a6b8
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-02-19 21:04:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import pandas as pd
-from text_mining_function import generate_review
+from textblob import TextBlob
 
 st.title("Generador de reseñas de libros")
 
@@ -8,7 +7,15 @@ uploaded_file = st.file_uploader("Cargar archivo de texto", type="txt")
 if uploaded_file is not None:
     # Leer el texto del libro cargado
     text = uploaded_file.read().decode("utf-8")
-    # Generar una reseña utilizando la función de text mining
-    review = generate_review(text)
+    # Analizar el sentimiento del texto del libro utilizando TextBlob
+    blob = TextBlob(text)
+    sentiment = blob.sentiment.polarity
+    # Generar una reseña basada en el sentimiento detectado
+    if sentiment > 0:
+        review = "Me gustó mucho este libro, ¡lo recomiendo!"
+    elif sentiment < 0:
+        review = "No me gustó este libro, no lo recomendaría."
+    else:
+        review = "Este libro es neutral, no tengo una opinión fuerte al respecto."
     # Mostrar la reseña generada al usuario
     st.write(review)
